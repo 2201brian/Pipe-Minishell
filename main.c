35767d34a7183597087fb8e24b4ef2bdbbed0d7e
Brian David Moreno <70359461+2201brian@users.noreply.github.com>
2020-10-30 23:48:26 -0500

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <sys/wait.h>
 
+#define READ  0
+#define WRITE 1
 
 int haysimbolopipe(char comando[]){
   if(strchr(comando, '|')!=NULL){
@@ -19,16 +21,16 @@ int haysimbolodireccion(char comando[]){
    return 0;
 }
 
-/*void separador(char comando[], char *comandoseparado[], char simbolo[]){
+void separador(char comando[], char *comandoseparado[], char simbolo[]){
   char * puntero;
-  int k = 0;
+  int i = 0;
   puntero = strtok(comando,simbolo);
-  while(puntero!=NULL){
+  while(i<2){
    comandoseparado[i] = puntero;
    puntero = strtok(NULL,simbolo);
    i++;
    }
-}*/
+}
 
 //wordCounter cuentas las palabras en una cadena, conciderandolas por espacio
 int wordCounter(char cadena[]){
@@ -66,11 +68,58 @@ void arrayChunks(char cadena[],char *cadenaVals[], int wc){
 
 void execute(char command[]){
    int size = strlen(command);
-   if (haysimbolopipe(command)==0 && haysimbolodireccion(command)==0){
+   if (haysimbolopipe(command)==0 && haysimbolodireccion(command)==0)
+   {
       int lon= wordCounter(command);
       char *vals[lon+1];
       arrayChunks(command,vals,lon);
       execvp(vals[0],vals);
+   }
+   else if (haysimbolopipe(command)==1 && haysimbolodireccion(command)==0)
+   {
+      char *comsPipe[2];
+      char pip[]="|";
+      separador(command,comsPipe,pip);
+
+      int lonL= wordCounter(comsPipe[0]);
+      int lonR= wordCounter(comsPipe[1]);
+      char *Lvals[lonL+1];
+      char *Rvals[lonR+1];
+      arrayChunks(comsPipe[0],Lvals,lonL);
+      arrayChunks(comsPipe[1],Rvals,lonR);
+
+      pid_t pid;
+      int fd[2];
+
+      if (pipe(fd)==-1){
+         perror("Creating pipe");
+         exit(EXIT_FAILURE);
+      }
+
+      pid = fork();
+      if (pid == -1){
+
+          perror("fork() failed xd");
+          exit(EXIT_FAILURE);
+
+      } else if (pid == 0){
+
+          //// Close the pipe write descriptor.
+          close(fd[WRITE]);
+          // Redirect STDIN to read from the pipe.
+          dup2(fd[READ], STDIN_FILENO);
+          // Execute
+          execvp(Rvals[0],Rvals);
+
+      } else {
+
+          // Close the pipe read descriptor.
+          close(fd[READ]);
+          // Redirect STDOUT to write to the pipe.
+          dup2(fd[WRITE], STDOUT_FILENO);
+          // Execute
+          execvp(Lvals[0],Lvals);
+      }
    }
 }
 
@@ -86,6 +135,7 @@ int main(){
                 pid_t pid = fork();
                 if(!pid){
                         execute(command);
+                        break;
                 }else{
                         wait(NULL);
                 }
